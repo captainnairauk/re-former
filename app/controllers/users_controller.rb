fd18294da2281d0b1c8b08f7b52907fdbfd516a7
Aniket Kaimal <aniket1995@yahoo.co.in>
2023-04-16 20:08:14 +0530

    Create a bare metal HTML form to create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
+
     if @user.save
-      redirect_to @user
+      redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
     end
+
   end
 
   private
